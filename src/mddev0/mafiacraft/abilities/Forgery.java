a2f9e4efc9bf6f19b9964903e703996a2f6229c2
MDdev0 <85643297+MDdev0@users.noreply.github.com>
2022-12-24 16:03:36 -0600

    Fix potential error if custom name is null

--- src/mddev0/mafiacraft/abilities/Forgery.java
+++ src/mddev0/mafiacraft/abilities/Forgery.java
@@ -2,12 +2,11 @@ package mddev0.mafiacraft.abilities;
 
 import mddev0.mafiacraft.MafiaCraft;
 import mddev0.mafiacraft.util.MafiaPlayer;
+import org.bukkit.ChatColor;
 import org.bukkit.entity.EntityType;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityPickupItemEvent;
-
-import java.util.Objects;
 
 public final class Forgery implements Listener {
 
@@ -20,8 +19,9 @@ public final class Forgery implements Listener {
     @EventHandler
     public void onItemPickup(EntityPickupItemEvent pickup) {
         if (!plugin.getActive()) return; // DO NOTHING IF NOT ACTIVE!
-        if (pickup.getEntityType() == EntityType.PLAYER &&
-                Objects.equals(pickup.getItem().getCustomName(), plugin.getConfig().getString("forgeItemName"))) {
+        String itemName = ChatColor.stripColor(pickup.getItem().getCustomName());
+        if (pickup.getEntityType() == EntityType.PLAYER && itemName != null &&
+                itemName.equalsIgnoreCase(plugin.getConfig().getString("forgeItemName"))) {
             // Check that thrower has ability
             MafiaPlayer thrower = plugin.getLivingPlayers().get(pickup.getItem().getThrower());
             MafiaPlayer toFrame = plugin.getLivingPlayers().get(pickup.getEntity().getUniqueId());
